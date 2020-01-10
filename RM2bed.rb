94b92ea832c394adea12b2689c6b5eeada280e1d
Michael G. Campana <campanam@si.edu>
2020-01-10 12:45:31 -0500

    Add files via upload

--- RM2bed.rb
+++ RM2bed.rb
@@ -2,20 +2,25 @@
 
 #----------------------------------------------------------------------------------------
 # RM2bed
-RM2BEDVER = "0.1.0"
+RM2BEDVER = "0.2.0"
 # Michael G. Campana, 2020
 # Smithsonian Conservation Biology Institute
 #----------------------------------------------------------------------------------------
 
-puts "chrom\tchromStart\tchromEnd"
-start = false
-File.open(ARGV[0]) do |f1|
-	while line = f1.gets
-		if start
-			line_arr = line.split(" ")
-			puts line_arr[4] + "\t" + (line_arr[5].to_i - 1).to_s + "\t" + line_arr[6]
-		elsif line == "\n"
-			start = true
+if ARGV[0].nil?
+	puts "\033[1mRM2bed " + RM2BEDVER + "\033[0m"
+	puts "\nUsage: ruby RM2bed.rb <in_RM.txt> > <out.bed>"
+else
+	puts "chrom\tchromStart\tchromEnd"
+	start = false
+	File.open(ARGV[0]) do |f1|
+		while line = f1.gets
+			if start
+				line_arr = line.split(" ")
+				puts line_arr[4] + "\t" + (line_arr[5].to_i - 1).to_s + "\t" + line_arr[6]
+			elsif line == "\n"
+				start = true
+			end
 		end
 	end
 end

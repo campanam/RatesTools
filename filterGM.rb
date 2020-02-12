71fe0796f4257cd9bf023cb17a3852f52b93bd88
Michael G. Campana <campanam@si.edu>
2020-02-12 15:04:02 -0500

    Add files via upload

--- filterGM.rb
+++ filterGM.rb
@@ -10,12 +10,16 @@ FILTERGMVER = "0.3.0"
 require_relative 'denovolib'
 
 if ARGV[0].nil?
-	format_splash('filterGM', FILTERGMVER, '<in_GenMap.bed[.gz]> <cutoff> > <out.bed>')
+	format_splash('filterGM', FILTERGMVER, '<in_GenMap.bed[.gz]> <cutoff> [exclude] > <out.bed>')
 else
 	gz_file_open(ARGV[0]).open(ARGV[0]) do |f1|
 		while line = f1.gets
 			line_arr = line.split
-			puts line if line_arr[4].to_f >= ARGV[1].to_f
+			if ARGV[2] == "exclude"
+				puts line if line_arr[4].to_f < ARGV[1].to_f
+			else
+				puts line if line_arr[4].to_f >= ARGV[1].to_f
+			end
 		end
 	end
 end

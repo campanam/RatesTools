e409a1252181bb1b77d8f021c7df457606ae0d44
Michael G. Campana <campanam@si.edu>
2020-12-16 16:47:16 -0500

    Code commented

--- bin/filterGM.rb
+++ bin/filterGM.rb
@@ -2,14 +2,18 @@
 
 #----------------------------------------------------------------------------------------
 # filterGM
-FILTERGMVER = "0.3.0"
+FILTERGMVER = "0.3.1"
 # Michael G. Campana, 2020
 # Smithsonian Conservation Biology Institute
 #----------------------------------------------------------------------------------------
 
+# Script to filter GenMap bed output for inclusion/exclusion of regions using VCFtools
+# ARGV[0] is input GenMap bed, ARGV[1] is cutoff float, ARGV[2] is optional, but 'exclude' converts to exclude BED (from default include)
+
 require_relative 'denovolib'
 
 if ARGV[0].nil?
+	# If no parameters passed, print help screen
 	format_splash('filterGM', FILTERGMVER, '<in_GenMap.bed[.gz]> <cutoff> [exclude] > <out.bed>')
 else
 	gz_file_open(ARGV[0]).open(ARGV[0]) do |f1|

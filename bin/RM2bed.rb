2825a2de0a46eb2ba979537b2c2aeb0af2f0d3d9
Michael G. Campana <campanam@si.edu>
2020-12-16 16:31:48 -0500

    Script commenting

--- bin/RM2bed.rb
+++ bin/RM2bed.rb
@@ -2,16 +2,20 @@
 
 #----------------------------------------------------------------------------------------
 # RM2bed
-RM2BEDVER = "0.4.0"
+RM2BEDVER = "0.4.1"
 # Michael G. Campana, 2020
 # Smithsonian Conservation Biology Institute
 #----------------------------------------------------------------------------------------
 
+# This script converts a RepeatMasker out file to a BED for exclusion of repeat regions using VCFtools.
+
 require_relative 'denovolib'
 
 if ARGV[0].nil?
+	# If no input RepeatMasker output, output usage using denovolib: format_splash
 	format_splash('RM2bed', RM2BEDVER, '<in_RM.txt[.gz]> > <out.bed>')
 else
+	# If provided a RepeatMasker out file, convert to a BED file for exclusion
 	start = false
 	gz_file_open(ARGV[0]).open(ARGV[0]) do |f1|
 		while line = f1.gets

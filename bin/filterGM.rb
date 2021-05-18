730aed099f1fe0d0a26d96187280396474105675
Michael G. Campana <campanam@si.edu>
2021-05-18 14:40:29 -0400

    Update filterGM.rb

--- bin/filterGM.rb
+++ bin/filterGM.rb
@@ -3,8 +3,19 @@
 #----------------------------------------------------------------------------------------
 # filterGM
 FILTERGMVER = "0.3.2"
-# Michael G. Campana, 2020
-# Smithsonian Conservation Biology Institute
+# Michael G. Campana and Ellie E. Armstrong, 2020
+# Smithsonian Institution and Stanford University
+
+# CC0: To the extent possible under law, the Smithsonian Institution and Stanford 
+# University have waived all copyright and related or neighboring rights to RatesTools;
+# this work is published from the United States. You should have received a copy of the
+# CC0 legal code along with this work. If not, see 
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+ 
+# We politely request that this work be cited as:
+# Campana, M.G. & E.E. Armstrong. 2020. RatesTools: Pipeline to calculate de novo
+# mutation rates from parent-offspring trios. Smithsonian Institution and Stanford
+# University. <https://github.com/campanam/RatesTools>.
 #----------------------------------------------------------------------------------------
 
 # Script to filter GenMap bed output for inclusion/exclusion of regions using VCFtools

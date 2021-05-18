fac11ea327595ed0fd5440d3b12a5804d11f2bfe
Michael G. Campana <campanam@si.edu>
2021-05-18 14:39:53 -0400

    Update calc_denovo_mutation_rate.rb

--- bin/calc_denovo_mutation_rate.rb
+++ bin/calc_denovo_mutation_rate.rb
@@ -3,8 +3,19 @@
 #----------------------------------------------------------------------------------------
 # calc_denovo_mutation_rate
 CALCDENOVOVER = "0.10.3"
-# Michael G. Campana, 2019-2020
-# Smithsonian Conservation Biology Institute
+# Michael G. Campana and Ellie E. Armstrong, 2019-2020
+# Smithsonian Institution and Stanford University
+
+# CC0: To the extent possible under law, the Smithsonian Institution and Stanford 
+# University have waived all copyright and related or neighboring rights to RatesTools;
+# this work is published from the United States. You should have received a copy of the
+# CC0 legal code along with this work. If not, see 
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+ 
+# We politely request that this work be cited as:
+# Campana, M.G. & E.E. Armstrong. 2020. RatesTools: Pipeline to calculate de novo
+# mutation rates from parent-offspring trios. Smithsonian Institution and Stanford
+# University. <https://github.com/campanam/RatesTools>.
 #----------------------------------------------------------------------------------------
 
 # This script calculates the de novo mutation rate from trios from a provided filtered VCF

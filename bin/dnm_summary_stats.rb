b80a23f5577d65c96ae0c090e5e523f688a55b05
Michael G. Campana <campanam@si.edu>
2023-11-02 11:06:37 -0400

    logstats bug fix

--- bin/dnm_summary_stats.rb
+++ bin/dnm_summary_stats.rb
@@ -2,7 +2,7 @@
 
 #----------------------------------------------------------------------------------------
 # dnm_summary_stats
-DNMSUMSTATSVER = "0.2.0"
+DNMSUMSTATSVER = "1.0.0"
 # Michael G. Campana and Ellie E. Armstrong, 2022-2023
 # Smithsonian Institution and Stanford University
 

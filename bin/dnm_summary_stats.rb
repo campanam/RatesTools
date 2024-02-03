b4246028a669996d2de5d1723cfcf6818557f090
Michael G. Campana <campanam@si.edu>
2024-02-02 22:52:25 -0500

    Update dnm_summary_stats.rb

--- bin/dnm_summary_stats.rb
+++ bin/dnm_summary_stats.rb
@@ -304,7 +304,7 @@ unless ARGV[3].nil? # Do not bother if no bootstrapped data
 		sf_corrected_ci = (sf_corrected_mean - sf_corrected_crit).to_s + '...' + (sf_corrected_mean + sf_corrected_crit).to_s
 		all_correction_factor = ($totalbases[key][1]-$total_removed[key]).to_f/$totalbases[key][1].to_f
 		all_corrected_mean = all_correction_factor * $bootstrapmeans[key][1]
-		all_corrected_se = all_corrected_mean/Math.sqrt(ARGV[3])
+		all_corrected_se = all_corrected_mean/Math.sqrt(ARGV[3].to_i)
 		all_corrected_crit = 1.96 * all_corrected_se
 		all_corrected_ci = (all_corrected_mean - all_corrected_crit).to_s + '...' + (all_corrected_mean + all_corrected_crit).to_s
 		puts key + "," + sf_corrected_mean.to_s + "," + sf_corrected_se.to_s + "," + sf_corrected_ci + "," + all_corrected_mean.to_s + ',' + all_corrected_se.to_s + ',' + all_corrected_ci
